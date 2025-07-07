cb98937ffbeb7b6c5d8fa03a158dd25c28f8dd1b
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2025-07-07 19:30:28 +0300

    initial commit

---
+++ src/RedotUpdaterServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Redot\Updater;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class RedotUpdaterServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot(): void
+    {
+        // ...
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register(): void
+    {
+        // ...
+    }
+}
